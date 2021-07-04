1d6437234bd0bf8d97100cdffb6fbf8b016112d4
Javis <javis@Javiss-Mac-mini.local>
2021-07-04 12:59:40 +0700

    update train

--- src/train_softmax.py
+++ src/train_softmax.py
@@ -1,4 +1,5 @@
-from sklearn.preprocessing import OneHotEncoder
+# from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import KFold
 from keras.models import load_model
@@ -29,8 +30,9 @@ le = LabelEncoder()
 labels = le.fit_transform(data["names"])
 num_classes = len(np.unique(labels))
 labels = labels.reshape(-1, 1)
-one_hot_encoder = OneHotEncoder(categorical_features = [0])
-labels = one_hot_encoder.fit_transform(labels).toarray()
+label_binarizer = LabelBinarizer()
+label_binarizer.fit(range(num_classes))
+labels = label_binarizer.transform(labels)
 
 embeddings = np.array(data["embeddings"])
 
@@ -43,10 +45,13 @@ input_shape = embeddings.shape[1]
 softmax = SoftMax(input_shape=(input_shape,), num_classes=num_classes)
 model = softmax.build()
 
+print(labels.shape)
+print(embeddings.shape)
 # Create KFold
 cv = KFold(n_splits = 5, random_state = 42, shuffle=True)
 history = {'acc': [], 'val_acc': [], 'loss': [], 'val_loss': []}
 # Train
+print('start training')
 for train_idx, valid_idx in cv.split(embeddings):
     X_train, X_val, y_train, y_val = embeddings[train_idx], embeddings[valid_idx], labels[train_idx], labels[valid_idx]
     his = model.fit(X_train, y_train,
@@ -61,6 +66,7 @@ for train_idx, valid_idx in cv.split(embeddings):
     history['loss'] += his.history['loss']
     history['val_loss'] += his.history['val_loss']
 
+print('finish training')
 
 # write the face recognition model to output
 model.save(args['model'])
